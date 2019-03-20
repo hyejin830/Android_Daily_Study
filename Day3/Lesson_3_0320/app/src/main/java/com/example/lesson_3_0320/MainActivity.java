3cd73aba429ff7c3bf52c44988dd5d1e043a546b
hyejin830 <hyejin_830@naver.com>
2019-03-20 14:02:54 +0900

    day3 commit

---
+++ Day3/Lesson_3_0320/app/src/main/java/com/example/lesson_3_0320/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lesson_3_0320;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText editid, editpw;
+    Button btn;
+    TextView textid, textpw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* 첫번째 예제용
+
+        editid = (EditText)findViewById(R.id.et_id);
+        editpw = (EditText)findViewById(R.id.et_pw);
+
+        btn = (Button)findViewById(R.id.btn_check);
+
+        textid = (TextView)findViewById(R.id.tv_id);
+        textpw = (TextView)findViewById(R.id.tv_pw);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btn_check:
+                        String id = editid.getText().toString();;
+                        String pw = editpw.getText().toString();
+
+                        textid.setText("입력하신 ID는 "+id+"입니다");
+                        textpw.setText("입력하신 PW는 "+pw+"입니다");
+                        break;
+                }
+            }
+        };
+
+        btn.setOnClickListener(listener);
+
+        */
+    }
+}
