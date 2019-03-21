7fdd101d239b03bb6016bfb256d5d7b873f5a01d
hyejin830 <hyejin_830@naver.com>
2019-03-21 17:50:17 +0900

    update commit

--- Day4/Lesson_4_0321/app/src/main/java/com/example/lesson_4_0321/MainActivity.java
+++ Day4/Lesson_4_0321/app/src/main/java/com/example/lesson_4_0321/MainActivity.java
@@ -4,192 +4,63 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
-    String InputNum = "";
-    double ResultNum=0.0;
-    int OpState = -1; // 0 : + , 1 : - , 2 : * , 3: / ,
-    EditText edInput;
-    TextView tvResult;
-    Button btOne,btTwo,btThree,btFour,btFive,btSix,btSeven, btEight,btNine,btZero;
-    Button btClear, btExecute, btPlus, btMinus, btDivide, btMultiple, btDot;
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
 
-        // 숫자 버튼
-        btOne = (Button)findViewById(R.id.bt_one);
-        btTwo = (Button)findViewById(R.id.bt_two);
-        btThree = (Button)findViewById(R.id.bt_three);
-        btFour = (Button)findViewById(R.id.bt_four);
-        btFive = (Button)findViewById(R.id.bt_five);
-        btSix = (Button)findViewById(R.id.bt_six);
-        btSeven = (Button)findViewById(R.id.bt_seven);
-        btEight = (Button)findViewById(R.id.bt_eight);
-        btNine = (Button)findViewById(R.id.bt_nine);
-        btZero = (Button)findViewById(R.id.bt_zero);
+        //첫번째 방법 - 익명 클래스
 
-        // 연산 버튼
-        btClear = (Button)findViewById(R.id.bt_clear);
-        btExecute = (Button)findViewById(R.id.bt_execute);
-        btPlus = (Button)findViewById(R.id.bt_plus);
-        btMinus = (Button)findViewById(R.id.bt_minus);
-        btDivide = (Button)findViewById(R.id.bt_divide);
-        btMultiple = (Button)findViewById(R.id.bt_multiple);
-        btDot = (Button)findViewById(R.id.bt_dot);
+        btn1.setOnClickListener(new Button.OnClickListener(){
 
-        // 나머지
-        edInput = (EditText) findViewById(R.id.et_input);
-        tvResult = (TextView)findViewById(R.id.tv_result);
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"버튼1 클릭",Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        View.OnClickListener listener = new View.OnClickListener() {
+        btn2.setOnClickListener(new Button.OnClickListener(){
 
-            String tmp;
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"버튼2 클릭",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //두번째 방법 -
+
+
+        View.OnClickListener listener = new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
                 switch (v.getId()){
-
-                    case R.id.bt_one :
-                        tmp = "1";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
+                    case R.id.btn1:
+                        Toast.makeText(getApplicationContext(),"버튼1 클릭",Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.bt_two :
-                        tmp = "2";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
+                    case R.id.btn2:
+                        Toast.makeText(getApplicationContext(),"버튼2 클릭",Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.bt_three :
-                        tmp = "3";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_four :
-                        tmp = "4";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_five :
-                        tmp = "5";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_six :
-                        tmp = "6";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_seven :
-                        tmp = "7";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_eight :
-                        tmp = "8";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_nine :
-                        tmp = "9";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_zero :
-                        tmp = "0";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-
-                        //연산 버튼 이벤트
-                    case R.id.bt_clear:
-                        InputNum ="";
-                        edInput.setText(InputNum);
-                        break;
-                    case R.id.bt_plus:
-                        tmp = "+";
-                        OpState = 0;
-                        edInput.setText(InputNum+tmp);
-                        ResultNum = Double.parseDouble(InputNum);
-                        InputNum ="";
-                        break;
-                    case R.id.bt_minus:
-                        tmp = "-";
-                        OpState = 1;
-                        edInput.setText(InputNum+tmp);
-                        ResultNum = Double.parseDouble(InputNum);
-                        InputNum ="";
-                        break;
-                    case R.id.bt_multiple:
-                        tmp = "*";
-                        OpState = 2;
-                        edInput.setText(InputNum+tmp);
-                        ResultNum = Double.parseDouble(InputNum);
-                        InputNum ="";
-                        break;
-                    case R.id.bt_divide:
-                        tmp = "/";
-                        OpState = 3;
-                        edInput.setText(InputNum+tmp);
-                        ResultNum = Double.parseDouble(InputNum);
-                        InputNum ="";
-                        break;
-                    case R.id.bt_dot:
-                        tmp = ".";
-                        edInput.setText(InputNum+tmp);
-                        InputNum = InputNum + tmp;
-                        break;
-                    case R.id.bt_execute:
-                        double ResultNum2 = Double.parseDouble(InputNum);
-                        if(OpState==0){
-                            ResultNum = ResultNum + ResultNum2;
-                        }else if(OpState==1){
-                            ResultNum = ResultNum - ResultNum2;
-                        }else if(OpState==2){
-                            ResultNum = ResultNum * ResultNum2;
-                        }else if(OpState==3){
-                            ResultNum = ResultNum / ResultNum2;
-                        }
-
-                        tvResult.setText(String.valueOf(ResultNum));
-                        InputNum ="";
-
-                        break;
-
                 }
             }
         };
 
-        btOne.setOnClickListener(listener);
-        btTwo.setOnClickListener(listener);
-        btThree.setOnClickListener(listener);
-        btFour.setOnClickListener(listener);
-        btFive.setOnClickListener(listener);
-        btSix.setOnClickListener(listener);
-        btSeven.setOnClickListener(listener);
-        btEight.setOnClickListener(listener);
-        btNine.setOnClickListener(listener);
-        btZero.setOnClickListener(listener);
+        btn1.setOnClickListener(listener);
+        btn2.setOnClickListener(listener);
 
-        btExecute.setOnClickListener(listener);
-        btClear.setOnClickListener(listener);
-        btPlus.setOnClickListener(listener);
-        btMinus.setOnClickListener(listener);
-        btMultiple.setOnClickListener(listener);
-        btDivide.setOnClickListener(listener);
-        btDot.setOnClickListener(listener);
-
-        edInput.setOnClickListener(listener);
-
-
-
+        */
 
     }
 }
+
--- Day4/Summary.md
+++ Day4/Summary.md
@@ -1,49 +1,140 @@
 
-1. Table Layout
+## Table Layout
 
-> 특징
- - 뷰들을 표 형태로 배치 
- - 예제(이미지1) 및 실습(이미지2) 
+> ##### 특징
+- 뷰들을 표 형태로 배치 
  
-> 태그
+> ##### 태그
  - TableRow : 행 생성, 내부 View들이 각 열
 
-> 속성
+> ##### 속성
  - android:layout_span : 열 병합
 
-2. Grid Layout
+> ##### 예제
+ 
+ <img src="https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/images/1.png" width="20%"></img>
+ 
+[코드로 이동](https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/text/TableLayout_example.txt)
 
-> 특징
+> ##### 실습
+
+ <img src="https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/images/2.png" width="20%"></img>
+
+[코드로 이동](https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/text/TableLayout_Practice.txt)
+
+## Grid Layout
+
+> ##### 특징
  - Table Layout과 유사하지만 더 직관적
- - 예제(이미지3)
 
-> 속성
- # GridLayout 내부
+> ##### 속성
+ ##### 1) GridLayout 내부
   - android:columnCount : 열의 수 지정
   - android:rowCount : 행의 수 지정
 
- # View 내부
+ ##### 2) View 내부
   - android:layout_column : 열의 위치 (시작-0)
   - android:layout_row : 행의 위치 (시작-0)
   - android:layout_columnSpan : 열 병합 
   - android:layout_rowSpan : 열 병합
 
+> ##### 예제
 
-3. 안드로이드 이벤트
+ <img src="https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/images/3.png" width="20%"></img>
+ 
+[코드로 이동](https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/text/GridLayout_Example.txt)
 
-> 설명
-  - 
+## EVENT
 
-> 리스너(Listener)란?
+>  ##### 설명
+  - 클릭 또는 드래그 등과 같은 동작들을 발생했을 때 이벤트가 발생
+  - 이벤트 처리 : 해당 동작들을 어떻게 처리하는 결정하는 것
+  - 이벤트 리스너(Event Listener) 객체를 생성하여 처리
+
+>  ##### 리스너(Listener)란?
   - "듣는 사람"인 것 처럼, 안드로이드에서 비슷한 개념
-  - 
+  - 어떤 이벤트의 발생 여부에 귀기울이는 객체를 의미
+  - View 클래스에 정의
 
-> 방법
+>  ##### 방법
 
  1) 익명(Anonymous) 클래스 생성
- 2) 
- 3) 
+   ```
+Button button1 = (Button) findViewById(R.id.button1) ;
+	button1.setOnClickListener(new Button.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+		// TODO : click event
+		}
+	});
+   ```
+   - 장점
+     - 이벤트 처리를 직관적으로 확인 가능
+     - 코드 작성이 간결
+     - 가장 많이 사용되는 방법
+   - 단점
+     - 위젯이 많아지면 코드가 길어진다
+     - 익명 클래스 외부에 있는 변수를 사용하려면 변수를 선언할 때 반드시 final 키워드 사용
  
+ 2) 익명(Anonymous) 클래스를 미리 만들고 참조하여 리스너 사용
+   
+   ```
+View.OnClickListener listener = new View.OnClickListener() {
+	@Override
+		public void onClick(View v) {
+			switch(v.getId()) {
+			case R.id.btn:
+				Toast.makeText(getApplicationContext(), "버튼1 선택", Toast.LENGTH_LONG).show();
+				if(checkBox.isChecked()) {
+				text.setText("체크박스가 체크되어있습니다.");
+				} else {
+				text.setText("체크박스가 체크되어있지 않습니다.");
+				}
+				break;
+			}
+		}
+	};
 
+        Button btn1 = (Button)findViewById(R.id.btn);
+        btn1.setOnClickListener(listener);
+   ```
+  - 익명 클래스 객체를 먼저 만들어 놓고 그 객체를 모든 위젯의 이벤트 리스너로 사용
+   - 매번 생성하지 않기 때문에 첫번째 방법의 문제점들을 해결
+ 
+3)  이벤트 리스너 인터페이스를 implements하는 이벤트 리스너 클래스를 생성하여 사용 
+  ```
+class BtnOnClickListener implements Button.OnClickListener {
+		@Override
+		public void onClick(View view) {
+			TextView textView1 = (TextView) findViewById(R.id.textView1);
+			switch (view.getId()) {
+
+			}
+		}
+	}
+  ```
+- 직접 클래스를 만들기 때문에 가독성이 높아짐
+ 
+4)  Layout 리소스 XML 에서 onClick 속성 이용
+
+ ```
+// 리소스 xml 
+<Button android:onClick="메소드명" />
+
+// java 코드
+public class MainActivity extends AppCompatActivity {
+    public void onButtonClick(View view) {
+    }
+...
+  ```
+  - 자주 사용하지 않는 방법
+
+## 계산기 실습
+
+> UI 구현
+
+ <img src="https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/images/4.png" width="20%"></img>
+ 
+[코드로 이동](https://github.com/hyejin830/Android_Daily_Study/blob/master/Day4/text/Calculator_xml.txt)
 
 
