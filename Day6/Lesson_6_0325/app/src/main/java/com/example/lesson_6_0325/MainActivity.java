6004b67dd9feed5969518bc6ff4f0a5ff237e032
hyejin830 <hyejin_830@naver.com>
2019-03-25 13:34:27 +0900

    first commit

--- Day6/Lesson_6_0325/app/src/main/java/com/example/lesson_6_0325/MainActivity.java
+++ Day6/Lesson_6_0325/app/src/main/java/com/example/lesson_6_0325/MainActivity.java
@@ -1,93 +1,18 @@
 package com.example.lesson_6_0325;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
-@Deprecated
-public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+public class MainActivity extends AppCompatActivity {
 
-    ActionBar.Tab tabSong;
-    ActionBar.Tab tabArtist;
-    ActionBar.Tab tabAlbum;
-    ActionBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bar = getActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        tabSong = bar.newTab();
-        tabSong.setText("음악별");
-        tabSong.setTabListener(this);
-        bar.addTab(tabSong);
-
-        tabArtist = bar.newTab();
-        tabArtist.setText("가수별");
-        tabArtist.setTabListener(this);
-        bar.addTab(tabArtist);
-
-        tabAlbum = bar.newTab();
-        tabAlbum.setText("앨범별");
-        tabAlbum.setTabListener(this);
-        bar.addTab(tabAlbum);
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
     }
 
 
-    public static class MyTabFragment extends android.support.v4.app.Fragment {
-        String tabName;
-        private MainActivity mainActivity;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            Bundle data = getArguments();
-            tabName = data.getString("tabName");
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-
-            LinearLayout baseLayout = new LinearLayout(super.getActivity());
-            baseLayout.setOrientation(LinearLayout.VERTICAL);
-            baseLayout.setLayoutParams(params);
-
-            if(tabName == "음악별"){
-                baseLayout.setBackgroundColor(Color.RED);
-            }
-            if(tabName == "가수별"){
-                baseLayout.setBackgroundColor(Color.GREEN);
-            }
-            if(tabName == "앨범별") {
-                baseLayout.setBackgroundColor(Color.BLUE);
-            }
-
-            return baseLayout;
-        }
-    }
 }
